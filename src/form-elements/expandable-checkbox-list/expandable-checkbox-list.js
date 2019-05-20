7ea7ce86ae1e56e82f8e3bef60e8ef71c6b65413
Alexandr Axenov <alexanderaxenov@mail.ru>
2019-05-20 17:27:13 +0800

    Edited block radio-buttons. Add block toggle-buttons

--- src/form-elements/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/form-elements/expandable-checkbox-list/expandable-checkbox-list.js
@@ -2,29 +2,8 @@ var jquery = require("jquery");
 window.$ = window.jQuery = jquery;
 require("jquery-ui-dist/jquery-ui.js");
 
-$(document).ready(function(){
-	function hideallDropdowns() {
-		$(".dropped .expandable-checkbox-list__submenu").hide();
-	}
-
-	function showDropdown(el) {
-        var el_li = $(el).parent().addClass('dropped');
-        el_li
-            .find('.title')
-            .click(function () {
-                hideallDropdowns();
-            })
-            .html($(el).html());
- 
-        el_li.find('.expandable-checkbox-list__submenu').show();
-	}
-	
-	$(".expandable-checkbox-list__menu").click(function(){
-        showDropdown(this);
-    });
- 
-    $(document).mouseup(function () {
-        hideallDropdowns();
-    });
- 
+$(".expandable-checkbox-list__menu").click(function(){
+	$(".expandable-checkbox-list__submenu").parent().children("ul").stop(true, false).fadeToggle(200);
+	// $(".expandable-checkbox-list__menu").css("background-image","url('./form-elements/expandable-checkbox-list/svg/expand_more.svg')")
+	// Не работает
 })
