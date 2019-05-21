bd95daa7f418134e07062eb97e9e7a85754bc00f
Alexandr Axenov <alexanderaxenov@mail.ru>
2019-05-21 12:44:24 +0800

    Add block rate-button

--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ import jquery from "jquery";
 
 import './main.scss';
 import './form-elements/expandable-checkbox-list/expandable-checkbox-list.js';
+import './form-elements/like-button/like-button.js';
 
