e1fd372665a92d54799ab9ee8df0490f028f12c1
Alexandr Axenov <alexanderaxenov@mail.ru>
2019-06-26 10:17:58 +0800

    Corrected expandable-checkbox-list

--- src/form-elements/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/form-elements/expandable-checkbox-list/expandable-checkbox-list.js
@@ -3,7 +3,5 @@ window.$ = window.jQuery = jquery;
 require("jquery-ui-dist/jquery-ui.js");
 
 $(".expandable-checkbox-list__menu").click(function(){
-	$(".expandable-checkbox-list__submenu").parent().children("ul").stop(true, false).fadeToggle(200);
-	// $(".expandable-checkbox-list__menu").css("background-image","url('./form-elements/expandable-checkbox-list/svg/expand_more.svg')")
-	// Не работает
+	$(".expandable-checkbox-list__submenu").stop(true, false).fadeToggle(200);
 })
