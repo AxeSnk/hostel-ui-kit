fa40c8d3d4e709dae2c1e7af997bdaa1194090a3
Alexandr Axenov <alexanderaxenov@mail.ru>
2019-06-26 12:21:01 +0800

    add mixin selDate

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import jquery from "jquery";
 
 import './main.scss';
-import './form-elements/dropdown-data/dropdown-data.js';
-import './form-elements/dropdown-data/datepicker-ru/datepicker-ru.js';
+import './form-elements/dropdown-filter/selection-dates/selection-dates.js';
+import './form-elements/dropdown-filter/selection-dates/datepicker-ru/datepicker-ru.js';
 import './form-elements/expandable-checkbox-list/expandable-checkbox-list.js';
 import './form-elements/like-button/like-button.js';
 import './form-elements/range-slider/range-slider.js';
