8ef2b7351ce3b49946420381dd883537b3107482
Alexandr Axenov <alexanderaxenov@mail.ru>
2019-06-27 14:39:19 +0800

    corrected classes name

---
+++ src/form-elements/form-elements.js
@@ -0,0 +1,7 @@
+import './field_masked/field_masked.js';
+import './dropdown-filter/selection-dates/selection-dates.js';
+import './dropdown-filter/selection-dates/datepicker-ru/datepicker-ru.js';
+import './expandable-checkbox-list/expandable-checkbox-list.js';
+import './like-button/like-button.js';
+import './range-slider/range-slider.js';
+import './dropdown_default/dropdown_default.js';
--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
 import jquery from "jquery";
 
 import './main.scss';
-import './form-elements/dropdown-filter/selection-dates/selection-dates.js';
-import './form-elements/dropdown-filter/selection-dates/datepicker-ru/datepicker-ru.js';
-import './form-elements/expandable-checkbox-list/expandable-checkbox-list.js';
-import './form-elements/like-button/like-button.js';
-import './form-elements/range-slider/range-slider.js';
-import './form-elements/dropdown_default/dropdown_default.js';
-
+import './form-elements/form-elements.js';
