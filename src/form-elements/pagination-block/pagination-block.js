7b70962422bb67991eaa005dab1c9fa10676c535
Alexandr Axenov <alexanderaxenov@mail.ru>
2019-07-12 15:42:38 +0800

    поправил pagination, добавил футер для search-room

--- src/form-elements/pagination-block/pagination-block.js
+++ src/form-elements/pagination-block/pagination-block.js
@@ -2,9 +2,10 @@ import './simplePagination.js';
 
   $('#pagination-demo').pagination({
     items: 15,
-    itemOnPage: 1,
-    currentPage: 2,
-    displayedPages: 5,
+    // itemsOnPage: 15,
+    currentPage: 1,
+    displayedPages: 3,
+    edges: 1,
     cssStyle: '',
     prevText: '<span aria-hidden="true"><i class="material-icons">arrow_back</i></span>',
     nextText: '<span aria-hidden="true"><i class="material-icons">arrow_forward</i></span>',
