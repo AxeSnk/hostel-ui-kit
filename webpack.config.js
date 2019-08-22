4fa2cec4a681be76b9abd517d5e28cbb9dc414b6
Alexandr Axenov <alexanderaxenov@mail.ru>
2019-08-22 11:18:36 +0800

    добавил copy-webpack-plugin для копирования favicon

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -42,7 +43,7 @@ module.exports = {
                 }
             },
             {
-                test: /\.(jpg|png|svg|gif)$/,
+                test: /\.(jpg|png|svg|gif|ico)$/,
                 exclude: [
                     path.resolve(__dirname, './src/fonts')
                 ],
@@ -144,6 +145,10 @@ module.exports = {
             template: './src/pages/room-details/room-details.scss',
             chunks: ['roomdetails']
         }),
+
+        new CopyPlugin([
+            { from: './src/favicon.ico', to: './src/img/' }
+        ]),
 
         new webpack.ProvidePlugin({
             $: 'jquery',
