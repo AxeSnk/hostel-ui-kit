920222339ab10e4d04193f5e52dce80d9bad8661
Alexander Axenov <alexanderaxenov@mail.ru>
2020-03-26 11:27:38 +0800

    add comment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ function importAll(resolve) {
   resolve.keys().forEach(resolve);
 }
 
-importAll(require.context('../src/', true, /\.(svg)$/)
+importAll(require.context('../src/', true, /\.(svg|jpg)$/)
 );
 importAll(require.context('../src/', true, /\.js$|\.scss$/));
