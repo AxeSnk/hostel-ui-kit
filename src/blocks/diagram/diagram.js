ead3fce3148a841bce217a8353f092d020736a90
Alexander Axcenov <alexanderaxenov@mail.ru>
2020-04-16 20:43:35 +0800

    responsive room-details

--- src/blocks/diagram/diagram.js
+++ src/blocks/diagram/diagram.js
@@ -6,7 +6,9 @@ var options = {
   },
   plotOptions: {
     pie: {
-      customScale: 1,
+      customScale: 1.08,
+      offsetX: -10,
+      offsetY: 5,
       donut: {
         size: '90%',
         labels: {
@@ -16,41 +18,43 @@ var options = {
           },
           value: {
             show: true,
-            color: '#BC9CFF',
-            formatter: function(val) {
-              return val + `\t` + 'голосов';
-            }
+            fontSize: '24px',
+            fontFamily: 'OpenSans, sans-serif',
+            fontWeight: 'bold',
+            color: '#BC9CFF'
+          },
+          total: {
+            show: true
           }
         }
       }
     }
   },
   series: [260, 260, 520],
-  labels: ['Великолепно', 'Хорошо', 'Удовлетворительно', 'Разочарован'],
+  labels: ['Удовлетворительно', 'Хорошо', 'Великолепно', 'Разочарован'],
   legend: {
+    fontSize: '14px',
+    fontFamily: 'Montserrat, sans-serif',
+    offsetY: 0,
     markers: {
+      width: 10,
+      height: 10,
       fillColors: ['#BC9CFF', '#6FCF97', '#FFE39C', '#919191']
+    },
+    itemMargin: {
+      horizontal: 0,
+      vertical: 4
     }
   },
   fill: {
-    colors: ['#BC9CFF', '#6FCF97', '#FFE39C', '#919191']
+    colors: ['#BC9CFF', '#6FCF97', '#FFE39C', '#919191'],
   },
   chart: {
     type: 'donut'
   },
-  responsive: [
-    {
-      breakpoint: 480,
-      options: {
-        chart: {
-          width: 200
-        },
-        legend: {
-          position: 'bottom'
-        }
-      }
-    }
-  ]
+  tooltip: {
+    enabled: false
+  }
 };
 
 var chart = new ApexCharts(document.querySelector('.diagram'), options);
