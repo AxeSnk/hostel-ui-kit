8de69fedf6a1e970839401038a96f90d5769a4af
Alexander Axcenov <alexanderaxenov@mail.ru>
2020-11-04 23:32:22 +0800

    #25 deleted a comment

--- src/blocks/checkbox-list/index.js
+++ src/blocks/checkbox-list/index.js
@@ -1,7 +1,3 @@
 import CheckboxList from './script/CheckboxList';
 
 document.querySelectorAll('.js-checkbox-list').forEach((root) => new CheckboxList(root));
-
-// $(document).ready(() => {
-//   $('.js-checkbox-list').each((i, root) => new CheckboxList(root));
-// });
